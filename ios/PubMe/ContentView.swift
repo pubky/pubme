9ec356d5bdec7909ba41e6c114fe422477a2c873
Jason <jayvdb1@gmail.com>
2024-08-01 16:09:36 +0200

    feat: pkarr resolving

--- ios/PubMe/ContentView.swift
+++ ios/PubMe/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var publicKey: String = "pk:7fmjpcuuzf54hw18bsgi3zihzyh4awseeuq5tmojefaezjbd64cy"
+    @State var record: String? = nil
+    @State var isResolving = false
+    @State var milliseconds: UInt64? = nil
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text(sayHi())
+        NavigationView {
+            Form {
+                Text("Public Key:")
+                TextField("Enter text", text: $publicKey)
+                
+                Button("Resolve") {
+                    record = nil
+                    milliseconds = nil
+                    isResolving = true
+                    let startTime = DispatchTime.now().uptimeNanoseconds
+                    
+                    Task {
+                        let record = await resolve(publicKey: publicKey)
+                        DispatchQueue.main.async {
+                            self.record = record
+                            isResolving = false
+                            milliseconds = (DispatchTime.now().uptimeNanoseconds - startTime)/1000000
+                        }
+                    }
+                }
+                .disabled(isResolving)
+                
+                if isResolving {
+                    ProgressView()
+                }
+                
+                if let milliseconds {
+                    Text("Resolved in \(milliseconds)ms")
+                }
+                
+                if let record {
+                    Text(record)
+                        .font(.caption)
+                }
+            }
+            .navigationTitle("Pkarr")
         }
-        .padding()
     }
 }
 
--- pubky-ffi/src/lib.rs
+++ pubky-ffi/src/lib.rs
@@ -1,6 +1,21 @@
 uniffi::setup_scaffolding!();
- 
+use pkarr::{PkarrClient, PublicKey};
+
 #[uniffi::export]
 fn say_hi() -> String {
     "Hello from Rust!".to_string()
+}
+
+#[uniffi::export]
+async fn resolve(public_key: String) -> String {
+    let client = PkarrClient::builder().build().unwrap();
+
+    let str: &str = &public_key;
+    let public_key: PublicKey = str.try_into().expect("Invalid zbase32 encoded key");
+    
+    match client.resolve(&public_key) {
+        Ok(Some(signed_packet)) => format!("{}", signed_packet),
+        Ok(None) => format!("Failed to resolve {}", str),
+        Err(e) => format!("Error resolving {}: {:?}", str, e),
+    }
 }
